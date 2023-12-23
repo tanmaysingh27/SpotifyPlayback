f8564c5f80ca80e94eff2e0782fdd07b9be6ba3d
tanmaysingh27 <111690894+tanmaysingh27@users.noreply.github.com>
2023-12-23 15:10:09 +0530

    Filtered out data
    Filtered out the necessary data from the JSON response from the Spotify API.

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import urequests as requests
 import ujson
 import network
-import spotipy 
+import utime
+import _thread
+import spotipy as spotify 
 
 authCode = 'authCode'
 ssid = "SSID"
-password = "password"
-acess_token = 'access_token'  #it is just made for the example 
+password = "Password"
 
-grant_type = 'authorization_code'
-
-#it is needed for some API calls
-redirect_uri = 'http://localhost:5000/callback'
-
-#get it from spotify dashboard
-client_id = 'client_id'
-client_secret = 'client_seceret'
-
-
+#Connecting to WiFi
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True);
 wifi.connect(ssid,password)
@@ -25,8 +17,9 @@ wifi.connect(ssid,password)
 if(wifi.isconnected()):
     print("Connected to: ", wifi.ifconfig()[0])
 
-#the access_token will be suplied through the api call made from the spotipy library I wrote to hanlde all the calls made for the api
-access_token = spotipy.token(authCode) 
+tokens = spotify.tokens(authCode)
+while true:
+    playback_state = spotify.playback_state(tokens['access_token'])
 
 
 
--- spotipy.py
+++ spotipy.py
@@ -9,8 +9,10 @@ client_secret = 'client_seceret'
 token_endpoint = 'https://accounts.spotify.com/api/token'
 player_endpoint = 'https://api.spotify.com/v1/me/player?market=IN'
 
+
 #This function is the first function to run, it generates the first authrization token and refresh token and passes to the main function
-def token(code):
+def tokens(code):
+    
     payload = {
     "grant_type": grant_type,
     "code": code,
@@ -25,10 +27,36 @@ def token(code):
     
     response_data = response.json()
     print(response_data['access_token'])
+    access_token = response_data['access_token']
+    print(response_data['refresh_token'])
+    refresh_token = response_data['refresh_token']
 
+    tokens = {'access_token':access_token,'refresh_token':refresh_token}
+    return tokens
 
-#This uses refresh token to generate a new access token to be used when expires 
+#This function uses the spotify endpoint to get the playback data
+def playback_state(access_token,):
+    
+    authorization ='Bearer '+access_token
+    headers = {"Authorization": authorization}
+    
+    response = requests.get(player_endpoint, headers = headers)
+    data = response.json()
+
+    playback_data = {
+        'deviceName':data['device']['name'],
+        'artist':data['item']['artists'][0]['name'],
+        'songName':data['item']['album']['name'],
+        'albumPic':data['item']['album']['images'][1],
+        'pause':data['is_playing'],
+        'progress':data['progress_ms'],
+        'duration':data['item']['duration_ms']        
+        }
+    return playback_data
+
+#This uses refresh token to generate a new access token to be used when expires     
 def referesh_token(refresh_token):
+    
     payload = {
     "grant_type": grant_type_refresh,
     "refresh_token": refresh_token,
@@ -43,13 +71,3 @@ def referesh_token(refresh_token):
     response_data = response.json()
     print(response_data['access_token'])
     
-
-#This function uses the spotify endpoint to get the playback data
-def playback_state(access_token):
-    authorization ='Bearer'+access_token
-    headers = {"Authorization": authorization}
-    response = requests.get(player_endpoint, headers = headers)
-    data = response.json()
-    print(data)
-
-        
